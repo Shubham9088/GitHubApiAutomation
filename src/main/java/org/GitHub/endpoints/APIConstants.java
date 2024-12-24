489fdb10e1b39c86f2af1cf4bcfde116d326da6a
Shubham9088 <shubhamchakole22@gmail.com>
2024-12-24 19:05:46 +0530

    user test cases

--- src/main/java/org/GitHub/endpoints/APIConstants.java
+++ src/main/java/org/GitHub/endpoints/APIConstants.java
@@ -3,5 +3,4 @@ package org.GitHub.endpoints;
 public class APIConstants {
     public static final String BASE_URL = "https://api.github.com";
     public static final String GetUserDetails="/user";
-
 }
