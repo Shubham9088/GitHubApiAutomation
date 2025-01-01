5e1bf4dde9ca5680f5dcd4ec35b18f4f29029e0a
Shubham9088 <shubhamchakole22@gmail.com>
2025-01-01 11:10:02 +0530

    user test cases

--- src/main/java/org/GitHub/endpoints/APIConstants.java
+++ src/main/java/org/GitHub/endpoints/APIConstants.java
@@ -3,7 +3,7 @@ package org.GitHub.endpoints;
 public class APIConstants {
     public static final String baseUrl = "https://api.github.com";
     public static final String getUserDetails ="/user";
-    public static final String token="Bearer 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";
+    public static final String token=System.getProperty("token");
     public static final String invalidToken="Bear abc";
     public static final String getAllBlockedUserList="/user/blocks";
     public static final String checkUserIsBlocked="/user/blocks/{username}";
